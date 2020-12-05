1407959601f88fabd91a4fb1e15ccb4fe3aa027b
Artmuntd <69253476+Artmuntd@users.noreply.github.com>
2020-12-06 00:48:45 +0300

    great folder

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const http = require('http');
+const http = require("http");
 const server = http.createServer(() => {
-    console.log('Пришёл запрос!');
+	console.log("Пришёл запрос!");
 });
 server.listen(3000);
