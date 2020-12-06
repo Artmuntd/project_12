7ec0aaf97617855420702617a791723f54dce178
Artmuntd <69253476+Artmuntd@users.noreply.github.com>
2020-12-07 00:38:26 +0300

    problems

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-const http = require("http");
-const server = http.createServer(() => {
-	console.log("Пришёл запрос!");
+const express = require('express');
+const path = require('path');
+const cardsUserRouter = require('./routes/cards');
+const userRouter = require('./routes/users');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+app.use('/', express.static(path.join(__dirname, 'public/html')));
+app.use('/users', userRouter);
+app.use('/cards', cardsUserRouter);
+
+app.use('*', (req, res) => {
+  res.status(404).json({ message: 'Запрашиваемый ресурс не найден' });
 });
-server.listen(3000);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
---
+++ routes/users.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const users = require('../data/user.json');
+
+router.get('/', (req, res) => {
+  res.send(users);
+});
+
+router.get('/:id', (req, res) => {
+  const user = users.find((u) => u._id === req.params.id);
+  if (user) {
+    res.send(user);
+    return;
+  }
+
+  res.status(404).json({ message: 'Такого пользователя не существует' });
+});
+
+module.exports = router;
